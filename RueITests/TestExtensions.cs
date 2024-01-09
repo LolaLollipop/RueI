6080eaa661bd0b30ae8f84f2077db93816ac982e
Ruemena <rue.heart.mena@gmail.com>
2024-01-09 16:58:02 -0600

    oops

--- RueITests/TestExtensions.cs
+++ RueITests/TestExtensions.cs
@@ -7,12 +7,5 @@ namespace RueITest;
 [TestClass]
 public class TestExtensions
 {
-    [TestMethod]
-    public void TestMax()
-    {
-        int maxOne = Math.Max(5, 3);
-        int maxTwo = 5.Max(3);
-        Assert.AreEqual(maxOne, maxTwo);
-    }
 }
 ///"\"hello world - - - again\""
